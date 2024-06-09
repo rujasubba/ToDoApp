5e0720c8bf2b5cd99c4ff1fa1e4ac733bfc4e11a
RujaSubba <rujasubba123@gmail.com>
2024-06-09 16:59:38 +0930

    new objects are created and pushed in an array. text decoration added when items are created

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
+const todoList = []
+
 function addTodoItem() {
     const inputElement = document.getElementById('new-todo-item')
     const inputElementValue = inputElement.value
 
+    const newTodoItem = {
+        text: inputElementValue,
+        completed: false
+    }
+
+   todoList.push(newTodoItem) 
+
+   updateTodoList()
+
+   function updateTodoList(){
+    const todoListElement = document.getElementById('todo-list')
+
+    todoListElement.innerHTML = ''
+    
+    for(const item of todoList){
+        const listItem = document.createElement('li')
+        listItem.textContent = item.text
+
+        listItem.addEventListener('click', () => {
+            // console.log("it has been clicked")
+            listItem.classList.add('completed')
+
+        } )
+
     
 
-    const todoListElement = document.getElementById('todo-list')
-    const listItem = document.createElement('li')
-    listItem.textContent = inputElement.value
+        todoListElement.appendChild(listItem)
 
-    todoListElement.appendChild(listItem)
+    }
 
-    const inputElementValue = ''
 
+    
+   }
+
+    
+
+
+ 
 
 }
